1fac0009336fd566b3181f4d4844d851fceb9469
Park Seonghyun <42666046+stupid-Park@users.noreply.github.com>
2018-09-29 15:58:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<algorithm>
 #include<vector>
+#include<string.h>
 using namespace std;
 struct classinfo
 {
@@ -29,17 +30,19 @@ classinfo c2[100];
 classinfo c3[100];
 vector<group> g2;
 vector<group> g3;
-int sn2,sn3;//subjectnumber
+int sn2,sn3,stn2,stn3;//subjectnumber
 bool cdevide(classinfo x,classinfo y)
 {
     if(x.snum/20>y.snum/20) return true;
     else if(x.snum/20<y.snum/20) return false;
     else if(x.ttime>y.ttime) return true;
+    else if(x.snum>y.snum) return true;
     else return false;
 }
 void devide4(int grade)
 {
     int i,j,n,s=-1,f;
+    group t1,t2,t3,t4;
     if(grade==2)
     {
         for(i=0;i<sn2;i++)
@@ -51,13 +54,14 @@ void devide4(int grade)
                 break;
             }
         }
-        for(i=s;i<f;i++)
+        for(i=s;i<f;i+=4)
         {
+            memset(&t1,0,sizeof(struct group));
+            memset(&t2,0,sizeof(struct group));
+            memset(&t3,0,sizeof(struct group));
+            memset(&t4,0,sizeof(struct group));
+            for(i=1;i<=stn2;i++)
             n=g2.size();
-            for(j=0;j<n;j++)
-            {
-
-            }
         }
     }
 }
@@ -66,6 +70,7 @@ int main()
     int i,j;
     //scan submit
     //scan classinfo_teacher, sbjcpde,ttime,floc,dloc
+    //scan stn2_2gradestudent,stn3_3gradestudent,sn2_2gradesubject,sn3_3gradesubject
     sort(c2,c2+sn,cdevide);
     sort(c3,c3+sn,cdevide);
     devide4(2);
